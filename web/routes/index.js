a9072163bc94d325264a14b3f8ea56a2064f85a4
Dahir Mohamed <dahir.mohamedd24@gmail.com>
2025-02-13 11:01:17 +0000

    ci-cd-basics-3: testing code-ql actions, fix

--- web/routes/index.js
+++ web/routes/index.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Nimble-ICE" });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Nimble-ICE' })
+})
 
 /* GET ice-flakes resource */
-router.get("/ice-flake", function (req, res, next) {
+router.get('/ice-flake', function (req, res, next) {
   res.status(201).json({
-    resource: "ice-flake",
+    resource: 'ice-flake',
     count: 1005,
-    shape: "circle",
-  });
-});
+    shape: 'circle'
+  })
+})
 
-module.exports = router;
+module.exports = router
